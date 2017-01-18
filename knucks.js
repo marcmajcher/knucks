5e624085eff75dff360df000e16a05def2627f25
Marc Majcher <marc.majcher@galvanize.com>
2017-01-18 16:30:52 -0600

    initial commit

---
+++ knucks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const months = [
+  'drug', 'hate', 'dick', 'real', 'dirt', 'thug', 'cash', 'girl', 'fist', 'shit', 'butt', 'kill'
+];
+
+const days = [
+  'city', 'riot', 'face', 'fear', 'grin', 'wolf', 'piss', 'rash', 'dogs', 'tits',
+  'lube', 'sand', 'fire', 'kids', 'bird', 'nerd', 'boys', 'moms', 'dads', 'vein',
+  'wurm', 'fart', 'trap', 'moan', 'hole', 'king', 'fuck', 'eyes', 'life', 'love', 'stab'
+];
